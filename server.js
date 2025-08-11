c1d5f810f4dda3779ce30091de20d31f65470c08
Casey Houle <c.j.houle75@gmail.com>
2025-08-11 10:20:23 -0700

    reduced to 10 lines

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const http = require("http");
 const HOST = "localhost";
-const PORT = 3000;
-
+const PORT = 4000;
 http
   .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Hello, Galvanize!");
   })
   .listen(PORT, HOST, () => {
